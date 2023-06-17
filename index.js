c60ed658ee3db12f4456499f4eef6f54f2580bf2
Devin <devincb93@gmail.com>
2023-06-17 16:35:24 -0700

    Ojects Lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employees = {
+    name: "Mike",
+    streetAddress: "110 Broadstreet",
+};
+function updateEmployeeWithKeyAndValue(employees, key, value) {
+    const copyOfEmployees = { ...employees};
+
+    copyOfEmployees[key] = value;
+
+    return copyOfEmployees;
+}
+const employee = updateEmployeeWithKeyAndValue(
+    employee,
+    "Sam",
+    "11 Broadway",
+)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+const streetAddress = destructivelyUpdateEmployeeWithKeyAndValue(employees, streetAddress, "12 Broadway");
+
+employees;
+
+function deleteFromEmployeeByKey(employees, key) {
+    const copyOfEmployees = { ...employees};
+
+    delete copyOfEmployees[key];
+    return copyOfEmployees;
+}
+function destructivelyDeleteFromEmployeeByKey(employees, key) {
+    delete employees[key];
+    return employees;
+}
